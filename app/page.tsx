14880c07da36f87d481c36bfc6281ac3dcba7078
Dian Ramadhani <105841116323@student.unismuh.ac.id>
2025-08-13 00:07:13 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,32 @@
-"use client";
-import { useRouter } from "next/navigation";
-
-export default function Home() {
-  const router = useRouter();
-
-  function handleLoginClick() {
-    router.push("/dashboard");
-  }
-
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+ 
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
-      <div className="bg-blue-600 w-full p-8 rounded-b-lg">
-        <h1 className="text-white text-4xl font-bold">🌐 Acme</h1>
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <AcmeLogo />
       </div>
-
-      <div className="mt-10 bg-white p-6 rounded-lg shadow-lg max-w-lg w-full text-center">
-        <h2 className="text-xl font-semibold mb-2">
-          Selamat datang di Acme.
-        </h2>
-        <p className="mb-6">
-          Ini adalah contoh untuk <a href="#" className="text-blue-500">Next.js Learn Course</a>, yang dibuat oleh Vercel.
-        </p>
-        <button
-          onClick={handleLoginClick}
-          className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition"
-        >
-          Log in →
-        </button>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Selamat datang di Acme.</strong> Ini adalah contoh untuk{' '}
+            <a href="https://nextjs.org/learn/" className="text-blue-500">
+              Next.js Learn Course
+            </a>
+            , yang dibuat oleh Vercel.
+          </p>
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+        </div>
       </div>
     </main>
   );
